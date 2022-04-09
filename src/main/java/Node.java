27f62fb405d6ea96b8ef81a5819fb40058ae42be
Pawel Wieczorek <236691@edu.p.lodz.pl>
2022-04-09 13:37:49 +0200

    implement Comparable

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -1,11 +1,16 @@
 import lombok.Getter;
 import lombok.Setter;
 
-public class Node {
+public class Node implements Comparable<Node> {
 
     public Node(int data, char character) {
         this.data = data;
         this.character = character;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return this.getData() - o.getData();
     }
 
     @Getter @Setter
