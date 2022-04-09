f7c8ddc75a05e2a14ed54113d4008a464ecee6ec
Pawel Wieczorek <236691@edu.p.lodz.pl>
2022-04-09 11:12:26 +0200

    add class contents

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -1,2 +1,24 @@
+import lombok.Getter;
+import lombok.Setter;
+
 public class Node {
+
+    public Node(int data, char character) {
+        this.data = data;
+        this.character = character;
+    }
+
+    @Getter @Setter
+    int data;
+
+    @Getter @Setter
+    private char character;
+
+    @Getter @Setter
+    private Node left;
+
+    @Getter @Setter
+    private Node right;
+
+
 }
