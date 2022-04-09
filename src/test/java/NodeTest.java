81ef65505e8bf5728a558712489b99c91ac88340
Pawel Wieczorek <236691@edu.p.lodz.pl>
2022-04-09 11:03:38 +0200

    add getters, setters, constructor

---
+++ src/test/java/NodeTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NodeTest {
+
+    @Test
+    void getDataTest() {
+        throw new UnsupportedOperationException("Test not implemented");
+    }
+
+    @Test
+    void getCharacterTest() {
+        throw new UnsupportedOperationException("Test not implemented");
+    }
+
+    @Test
+    void getLeftTest() {
+        throw new UnsupportedOperationException("Test not implemented");
+    }
+
+    @Test
+    void getRightTest() {
+        throw new UnsupportedOperationException("Test not implemented");
+    }
+
+    @Test
+    void setDataTest() {
+        throw new UnsupportedOperationException("Test not implemented");
+    }
+
+    @Test
+    void setCharacterTest() {
+        throw new UnsupportedOperationException("Test not implemented");
+    }
+
+    @Test
+    void setLeftTest() {
+        throw new UnsupportedOperationException("Test not implemented");
+    }
+
+    @Test
+    void setRightTest() {
+        throw new UnsupportedOperationException("Test not implemented");
+    }
+}
