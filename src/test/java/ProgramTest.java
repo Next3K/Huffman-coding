e50ed142ba6e768e36949cbf2571a597f76eff38
Pawel Wieczorek <236691@edu.p.lodz.pl>
2022-04-10 12:58:18 +0200

    implemet test methods

--- src/test/java/ProgramTest.java
+++ src/test/java/ProgramTest.java
@@ -4,13 +4,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ProgramTest {
 
+    private final String message = "000001011";
+    private final String frequencies = "D0000000000000100C0000000000000011B0000000000000010A0000000000000001";
+
     @Test
     void completeInputEncoding() {
-        throw new UnsupportedOperationException("Test not implemented");
+        assertEquals(message,Program.encode("DDDDCCCBBA").getKey());
+        assertEquals(frequencies,Program.encode("DDDDCCCBBA").getValue());
     }
 
     @Test
     void completeInputDecoding() {
-        throw new UnsupportedOperationException("Test not implemented");
+        assertEquals("DDDDCCCBBA",Program.decode(frequencies,message));
     }
 }
