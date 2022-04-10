ff83ab87c715abb49c7c49fb87cba71c63c41c49
Pawel Wieczorek <236691@edu.p.lodz.pl>
2022-04-10 14:02:59 +0200

    implement methods to satisfy tests

--- src/main/java/IOController.java
+++ src/main/java/IOController.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class IOController {
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class IOController {
+
+    public static String read(String filename) throws IOException {
+        File input = new File("inputFiles" + File.separator + filename + ".txt");
+        return new String(Files.readAllBytes(Paths.get(input.getAbsolutePath())));
+    }
+
+    public static void write(String filename, String text) throws IOException {
+        File output = new File("outputFiles" + File.separator + filename + ".txt");
+        Files.write(Paths.get(output.getAbsolutePath()), text.getBytes());
+    }
 }
