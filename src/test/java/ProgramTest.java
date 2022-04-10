83275be7d5f34dda71f1ecce54ec5f776047ea63
Pawel Wieczorek <236691@edu.p.lodz.pl>
2022-04-10 13:24:00 +0200

    removed errors in test methods

--- src/test/java/ProgramTest.java
+++ src/test/java/ProgramTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ProgramTest {
 
-    private final String message = "0000010010101011111";
+    private final String message = "0000101010111111110";
     private final String frequencies = "D0000000000000100C0000000000000011B0000000000000010A0000000000000001";
 
     private final String message2 = "11111111111111111111";  // 20 times letter 'A'
@@ -13,8 +13,8 @@ class ProgramTest {
 
     @Test
     void completeInputEncoding_when_input_has_multiple_letters() {
-        assertEquals(message,Program.encode("DDDDCCCBBA").getKey());
-        assertEquals(frequencies,Program.encode("DDDDCCCBBA").getValue());
+        assertEquals(frequencies,Program.encode("DDDDCCCBBA").getKey());
+        assertEquals(message,Program.encode("DDDDCCCBBA").getValue());
     }
 
     @Test
@@ -24,8 +24,8 @@ class ProgramTest {
 
     @Test
     void completeInputEncoding_when_input_has_one_letter() {
-        assertEquals(message2,Program.encode("AAAAAAAAAAAAAAAAAAAA").getKey());
-        assertEquals(frequencies2,Program.encode("AAAAAAAAAAAAAAAAAAAA").getValue());
+        assertEquals(frequencies2,Program.encode("AAAAAAAAAAAAAAAAAAAA").getKey());
+        assertEquals(message2,Program.encode("AAAAAAAAAAAAAAAAAAAA").getValue());
     }
 
     @Test
