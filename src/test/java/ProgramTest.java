3581ff50bcd36c1059f06a984d77b260b0a50ee9
Pawel Wieczorek <236691@edu.p.lodz.pl>
2022-04-10 13:08:35 +0200

    add test methods

--- src/test/java/ProgramTest.java
+++ src/test/java/ProgramTest.java
@@ -4,17 +4,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ProgramTest {
 
-    private final String message = "000001011";
+    private final String message = "0000010010101011111";
     private final String frequencies = "D0000000000000100C0000000000000011B0000000000000010A0000000000000001";
 
+    private final String message2 = "11111111111111111111";  // 20 times letter 'A'
+    private final String frequencies2 = "A0000000000010100"; // 20 times letter 'A'
+
+
     @Test
-    void completeInputEncoding() {
+    void completeInputEncoding_when_input_has_multiple_letters() {
         assertEquals(message,Program.encode("DDDDCCCBBA").getKey());
         assertEquals(frequencies,Program.encode("DDDDCCCBBA").getValue());
     }
 
     @Test
-    void completeInputDecoding() {
+    void completeInputDecoding_when_input_has_multiple_letters() {
         assertEquals("DDDDCCCBBA",Program.decode(frequencies,message));
+    }
+
+    @Test
+    void completeInputEncoding_when_input_has_one_letter() {
+        assertEquals(message2,Program.encode("AAAAAAAAAAAAAAAAAAAA").getKey());
+        assertEquals(frequencies2,Program.encode("AAAAAAAAAAAAAAAAAAAA").getValue());
+    }
+
+    @Test
+    void completeInputDecoding_when_input_has_one_letter() {
+        assertEquals("AAAAAAAAAAAAAAAAAAAA",Program.decode(frequencies2,message2));
     }
 }
