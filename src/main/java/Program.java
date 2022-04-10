7c3885ae616e29a3d044d769e78fc45fad33bb4d
Pawel Wieczorek <236691@edu.p.lodz.pl>
2022-04-10 12:58:32 +0200

    add comments

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,9 +1,23 @@
+import javafx.util.Pair;
+
 public class Program {
-    public static String completeInputEncoding(String text) {
+
+    /**
+     * Encodes text with Huffman tree.
+     * @param text text to encode.
+     * @return Pair of strings: frequency table, encoded message.
+     */
+    public static Pair<String,String> encode(String text) {
         return null;
     }
 
-    public static String completeInputDecoding(String text) {
+    /**
+     * Decodes previously encoded text using frequency table.
+     * @param frequencyTable String representing frequency table.
+     * @param text encoded text.
+     * @return decoded text.
+     */
+    public static String decode(String frequencyTable, String text) {
         return null;
     }
 }
